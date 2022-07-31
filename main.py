e4f21cc7fa72cc6c0a7db00145180b202b3301cc
andrew <andrew.l.foy@gmail.com>
2022-07-31 19:23:31 +0100

    very important change yup

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import discord
 from config import token
 
-print(token)
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('bot is logged in')
+    activity = discord.Streaming(name="BARK BARK", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ", type = "3")
+    await client.change_presence(status=discord.Status.idle, activity=activity)
+
+client.run(token)
