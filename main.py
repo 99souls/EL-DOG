96301d61a22acbf1839261cd699ddfe165e66811
andrew <andrew.l.foy@gmail.com>
2022-07-31 20:58:10 +0100

    made changes to git-ignore

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ async def on_ready():
     print('bot is logged in')
     activity = discord.Streaming(name="BARK BARK", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ", type = "3")
     await client.change_presence(status=discord.Status.idle, activity=activity)
-
+    
 client.run(token)
